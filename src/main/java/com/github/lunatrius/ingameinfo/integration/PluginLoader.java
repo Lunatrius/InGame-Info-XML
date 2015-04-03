f7e5ddb3104d10b998a34a7eb31c76d2ec817ea7
Lunatrius <jadran.kotnik@gmail.com>
2015-04-03 19:10:14 +0200

    Replaced @Mod classes with integration plugins.

---
+++ src/main/java/com/github/lunatrius/ingameinfo/integration/PluginLoader.java
@@ -0,0 +1,68 @@
+package com.github.lunatrius.ingameinfo.integration;
+
+import com.github.lunatrius.ingameinfo.reference.Reference;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PluginLoader {
+    private final List<Plugin> plugins = new ArrayList<Plugin>();
+
+    private static PluginLoader instance = null;
+
+    public static PluginLoader getInstance() {
+        if (instance == null) {
+            instance = new PluginLoader();
+        }
+
+        return instance;
+    }
+
+    public void preInit(final FMLPreInitializationEvent event) {
+        final String[] names = new String[] {
+                "com.github.lunatrius.ingameinfo.integration.bloodmagic.BloodMagic",
+                "com.github.lunatrius.ingameinfo.integration.simplyjetpacks.SimplyJetpacks",
+                "com.github.lunatrius.ingameinfo.integration.terrafirmacraft.TerraFirmaCraft",
+                "com.github.lunatrius.ingameinfo.integration.thaumcraft.Thaumcraft"
+        };
+
+        for (final String name : names) {
+            try {
+                final Class<?> clazz = Class.forName(name);
+
+                if (!Plugin.class.isAssignableFrom(clazz)) {
+                    continue;
+                }
+
+                final Class<? extends Plugin> clazzPlugin = (Class<? extends Plugin>) clazz;
+                final Plugin plugin = clazzPlugin.newInstance();
+                if (plugin.canLoad()) {
+                    this.plugins.add(plugin);
+                }
+            } catch (final ClassNotFoundException cnfe) {
+                Reference.logger.error("{} not found, skipping...", name);
+            } catch (final InstantiationException ie) {
+                Reference.logger.error("{} could not be created, skipping...", name);
+            } catch (final IllegalAccessException iae) {
+                Reference.logger.error("{} could not be accessed, skipping...", name);
+            }
+        }
+
+        if (this.plugins.size() > 0) {
+            event.getModMetadata().description += "\nCompiled against " + this.plugins;
+        }
+    }
+
+    public void postInit(final FMLPostInitializationEvent event) {
+        for (final Plugin plugin : this.plugins) {
+            plugin.load();
+
+            Reference.logger.debug("Loaded {} integration.", plugin.getDependencyName());
+        }
+
+        // all plugins loaded, let it go~
+        instance = null;
+    }
+}
--- src/main/java/com/github/lunatrius/ingameinfo/integration/bloodmagic/BloodMagic.java
+++ src/main/java/com/github/lunatrius/ingameinfo/integration/bloodmagic/BloodMagic.java
@@ -1,29 +1,23 @@
 package com.github.lunatrius.ingameinfo.integration.bloodmagic;
 
+import com.github.lunatrius.ingameinfo.integration.Plugin;
 import com.github.lunatrius.ingameinfo.integration.bloodmagic.tag.TagBloodMagic;
 import com.github.lunatrius.ingameinfo.reference.Names;
-import com.github.lunatrius.ingameinfo.reference.Reference;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Loader;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.relauncher.Side;
 
-@Mod(modid = BloodMagic.MODID, name = BloodMagic.NAME, version = Reference.VERSION, dependencies = BloodMagic.DEPENDENCIES)
-public class BloodMagic {
-    public static final String MODID = Reference.MODID + "|" + Names.Mods.BLOODMAGIC_MODID + "Integration";
-    public static final String NAME = Reference.NAME + " - " + Names.Mods.BLOODMAGIC_NAME + " Integration";
-    public static final String DEPENDENCIES = "after:" + Reference.MODID + ";after:" + Names.Mods.BLOODMAGIC_MODID;
+@SuppressWarnings("UnusedDeclaration")
+public class BloodMagic extends Plugin {
+    @Override
+    public String getDependency() {
+        return Names.Mods.BLOODMAGIC_MODID;
+    }
 
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-        if (FMLCommonHandler.instance().getEffectiveSide() != Side.CLIENT) {
-            return;
-        }
+    @Override
+    public String getDependencyName() {
+        return Names.Mods.BLOODMAGIC_NAME;
+    }
 
-        if (Loader.isModLoaded(Names.Mods.BLOODMAGIC_MODID)) {
-            TagBloodMagic.register();
-        }
+    @Override
+    public void load() {
+        TagBloodMagic.register();
     }
 }
